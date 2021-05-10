361778b000f21f91721ccb005620f43ffa0f6979
Luca Martinelli <lucxmangajet@gmail.com>
2021-05-10 20:32:06 +0200

    added newer tests from practice

--- UnityProject/Assets/Scripts/Tests/ExampleTests.cs
+++ UnityProject/Assets/Scripts/Tests/ExampleTests.cs
@@ -14,11 +14,41 @@ public class ExampleTests
         Vector3 enemy1 = new Vector3(1, 0, 0);
         Vector3 enemy2 = new Vector3(2, 0, 0);
 
-        Vector3 result = ExampleModelCode.FindClosestPositionTo(player, new Vector3[] { enemy1, enemy2 });
+        Vector3 result = ExampleModelCode.FindClosestPositionTo(player,
+            new Vector3[] { enemy1, enemy2 });
 
         Assert.AreEqual(enemy1, result, "FindClosestPosition failed.");
     }
 
+    [Test]
+    public void HoangTestArbitraryAmount()
+    {
+        Vector3 player = Vector3.zero;
+        int n = 1000;
+
+        Vector3[] enemies = new Vector3[n];
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].x = i + 69;
+        }
+
+        Vector3 result = ExampleModelCode.FindClosestPositionTo(player, enemies);
+
+        Assert.AreEqual(enemies[0], result, "FindClosestPosition failed.");
+    }
+
+    [Test]
+    public void EnemyInfinitelyFarAwayTest()
+    {
+        Vector3 player = Vector3.zero;
+        Vector3 infiniteEnemy = Vector3.positiveInfinity;
+
+        Vector3 result = ExampleModelCode.FindClosestPositionTo(player,
+            new Vector3[] { infiniteEnemy });
+
+        Assert.AreEqual(infiniteEnemy, result);
+    }
 
     [Test]
     public void NoEnemiesTest()
